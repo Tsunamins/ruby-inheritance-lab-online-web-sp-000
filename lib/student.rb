9ed030907b2272fed21dfb2e044d93c96c855303
Alison Reilly <reillyamr@gmail.com>
2019-01-05 00:22:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,19 @@
-require_relative './user'
+require_relative './user.rb'
 
-class Student 
+class Student < User 
+  attr_accessor :knowledge
+  
+  def initialize
+    @knowledge = []
+  end 
+  
+  def learn(string) 
+    @knowledge << string 
+  end 
+  
+  def knowledge 
+    @knowledge 
+  end 
+
 
 end
